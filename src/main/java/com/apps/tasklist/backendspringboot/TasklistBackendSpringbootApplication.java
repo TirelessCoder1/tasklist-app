d5b0e26d0d9042bc97c34b9ac390bd112474adb8
Artem Zakharchuk <artem.zaharchuk1@gmail.com>
2022-05-13 12:35:19 +0300

    Initial commit

---
+++ src/main/java/com/apps/tasklist/backendspringboot/TasklistBackendSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.apps.tasklist.backendspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasklistBackendSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TasklistBackendSpringbootApplication.class, args);
+    }
+
+}

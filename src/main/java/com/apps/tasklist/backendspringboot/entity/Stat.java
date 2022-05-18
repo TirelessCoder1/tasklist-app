d2d54f769e1a799201b271b571d6b9b4ca5e3a08
Artem Zakharchuk <artem.zaharchuk1@gmail.com>
2022-05-19 00:55:17 +0300

    Refactor entity files and remove junk code using Lombok
    Auto-generated entity classes were created with some code, which I ain`t gonna use. Removed these line. Additionally replaced equals, hash methods and setters with Lombok annotations to improve code readability

--- src/main/java/com/apps/tasklist/backendspringboot/entity/Stat.java
+++ src/main/java/com/apps/tasklist/backendspringboot/entity/Stat.java
@@ -1,65 +1,39 @@
 package com.apps.tasklist.backendspringboot.entity;
 
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
+@NoArgsConstructor
+@EqualsAndHashCode
+@Setter
 public class Stat {
+
+    //annotation for getting 'id' when object is added
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
-    @Column(name = "id", nullable = false)
+    @Column(name = "id")
     private Long id;
     @Basic
-    @Column(name = "completed_total", nullable = true)
+    @Column(name = "completed_total")
     private Long completedTotal;
     @Basic
-    @Column(name = "uncompleted_total", nullable = true)
+    @Column(name = "uncompleted_total")
     private Long uncompletedTotal;
 
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public Long getCompletedTotal() {
         return completedTotal;
-    }
-
-    public void setCompletedTotal(Long completedTotal) {
-        this.completedTotal = completedTotal;
     }
 
     public Long getUncompletedTotal() {
         return uncompletedTotal;
     }
 
-    public void setUncompletedTotal(Long uncompletedTotal) {
-        this.uncompletedTotal = uncompletedTotal;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Stat stat = (Stat) o;
-
-        if (id != null ? !id.equals(stat.id) : stat.id != null) return false;
-        if (completedTotal != null ? !completedTotal.equals(stat.completedTotal) : stat.completedTotal != null)
-            return false;
-        if (uncompletedTotal != null ? !uncompletedTotal.equals(stat.uncompletedTotal) : stat.uncompletedTotal != null)
-            return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (completedTotal != null ? completedTotal.hashCode() : 0);
-        result = 31 * result + (uncompletedTotal != null ? uncompletedTotal.hashCode() : 0);
-        return result;
-    }
 }
